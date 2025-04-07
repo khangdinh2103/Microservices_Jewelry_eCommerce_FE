f395e62bc20c3913183b1f7ad46824b9e3da953d
Khang Dinh <21123341.khang@student.iuh.edu.vn>
2025-04-08 02:19:42 +0700

    Them tinh khoảng cách, thoi gian

--- Workspace/Service_Cart_Order/Service_Cart_Order_Frontend/src/screens/Checkout.tsx
+++ Workspace/Service_Cart_Order/Service_Cart_Order_Frontend/src/screens/Checkout.tsx
@@ -4,6 +4,7 @@ import PaymentMethodSelector from "../components/PaymentMethodSelector";
 import { useNavigate } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Polyline, Popup, useMapEvents } from "react-leaflet";
 import LocationPicker from "../components/PickerLocation";
+import { getCurrentLocation } from "../components/PickerLocation";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 const customIcon = new L.Icon({
@@ -217,30 +218,38 @@ const Checkout: React.FC = () => {
       console.error(error);
     }
   };
-  const handleGetCurrentLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        const lat = position.coords.latitude;
-        const lng = position.coords.longitude;
-        setMapLocation({ lat, lng });
+  // const handleGetCurrentLocation = () => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(async (position) => {
+  //       const lat = position.coords.latitude;
+  //       const lng = position.coords.longitude;
+  //       setMapLocation({ lat, lng });
 
-        try {
-          const response = await fetch(`http://localhost:3000/api/location?lat=${lat}&lng=${lng}`);
-          const data = await response.json();
-          if (data.address) {
-            setUserInfo((prev) => ({ ...prev, address: data.address }));
-          } else {
-            alert("Không thể lấy địa chỉ!");
-          }
-        } catch (error) {
-          console.error("Lỗi lấy vị trí:", error);
-        }
-      }, (error) => {
-        alert("Không thể truy cập vị trí! Vui lòng kiểm tra cài đặt trình duyệt.");
-      });
-    } else {
-      alert("Trình duyệt không hỗ trợ lấy vị trí!");
-    }
+  //       try {
+  //         const response = await fetch(`http://localhost:3000/api/location?lat=${lat}&lng=${lng}`);
+  //         const data = await response.json();
+  //         if (data.address) {
+  //           setUserInfo((prev) => ({ ...prev, address: data.address }));
+  //         } else {
+  //           alert("Không thể lấy địa chỉ!");
+  //         }
+  //       } catch (error) {
+  //         console.error("Lỗi lấy vị trí:", error);
+  //       }
+  //     }, (error) => {
+  //       alert("Không thể truy cập vị trí! Vui lòng kiểm tra cài đặt trình duyệt.");
+  //     });
+  //   } else {
+  //     alert("Trình duyệt không hỗ trợ lấy vị trí!");
+  //   }
+  // };
+  const handleGetCurrentLocation = () => {
+    getCurrentLocation({
+      setMapLocation,
+      setUserInfo,
+      setRoute,
+      setRouteInfo,
+    });
   };
   
   return (
