ed012f45c4849b31fccd581af4af51d2eb247dba
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-05-11 02:28:51 +0900

    update: config container

--- Workspace_Final/Container/container-vite/vite.config.ts
+++ Workspace_Final/Container/container-vite/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	server: {
+		port: 3000,
+	},
+    plugins: [
+		react(), 
+		tailwindcss(),
+		federation({
+			name: 'container',
+			remotes: {
+				
+			},
+			shared: ["react", "react-dom"]
+		})
+	],
+});
