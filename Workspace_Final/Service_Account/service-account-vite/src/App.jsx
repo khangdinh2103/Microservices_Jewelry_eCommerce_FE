442dcf8bc07ca35dbd03267948320b1f762bbb48
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-05-14 04:08:10 +0900

    delete: unnecessary

--- Workspace_Final/Service_Account/service-account-vite/src/App.jsx
+++ Workspace_Final/Service_Account/service-account-vite/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from 'container/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
