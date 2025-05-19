1d4e7328b3f67e8a3c7f96d67aa573936f7a1474
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-05-20 02:43:35 +0900

    fix: responsive css

--- Workspace_Final/Container/container-vite/src/components/Chatbot.tsx
+++ Workspace_Final/Container/container-vite/src/components/Chatbot.tsx
@@ -103,7 +103,7 @@ const ChatBot: React.FC<ChatBotProps> = ({onClose}) => {
 
     return (
         <div
-            className="fixed bottom-2 left-2 md:left-6 md:bottom-6 w-1/4 md:w-96 h-[500px] md:h-[550px] bg-white rounded-t-lg md:rounded-lg shadow-2xl flex flex-col z-50 overflow-hidden border border-gray-100">
+            className="fixed bottom-0 left-0 md:left-6 md:bottom-6 w-full md:w-96 h-[500px] md:h-[550px] bg-white rounded-t-lg md:rounded-lg shadow-2xl flex flex-col z-50 overflow-hidden border border-gray-100">
             {/* Header */}
             <div
                 className="bg-gradient-to-r from-amber-600 to-amber-700 text-white px-4 py-3 flex justify-between items-center">
